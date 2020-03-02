0d007fbc8ad018a55d81acca21a71a51572f3714
CynthiaGathoniWainaina <cynthiagathoniwainaina@gmail.com>
2020-03-02 12:50:40 +0300

    project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LandingPageComponent} from './components/landing-page/landing-page.component';
+import {SignupComponent} from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  // Landing Page
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },
+  { path: 'landing', component: LandingPageComponent },
+
+  { path: 'signup', component: SignupComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
